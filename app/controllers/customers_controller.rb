e2a304722d8e95d71e1b3a94572f933b50b1ca35
Samuel Cornet <samuel@noukod.com>
2019-02-26 18:57:49 +0000

    Automatically backed up by Learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,9 @@
 class CustomersController < ApplicationController
- 
+
   def index
     @customers = Customer.all
   end
- 
+
   def upload
     LeadsWorker.perform_async(params[:leads].path)
     redirect_to customers_path
